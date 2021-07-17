cb5b4526265f9a7087fe91ed8ad45cf30d6547c5
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-07-17 23:17:13 +0700

    add migration file, update models and use meta options in django models

--- CourseApp/courseapp/courses/models.py
+++ CourseApp/courseapp/courses/models.py
@@ -15,12 +15,36 @@ class Category(models.Model):   #courses_category
         return self.name
 
 
-class Course(models.Model):
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True #Khi tạo bảng trong CSDL thì các lớp kế thừa lớp này sẽ được tạo thành các bảng có chứa các thuộc tính của lớp kế thừa, còn lớp kế thừa sẽ không tạo thành bảng
+
     subject = models.CharField(max_length=255, null=False)
+    image = models.ImageField(upload_to='courses/%Y/%m', default=None)
+    created_date = models.DateTimeField(auto_now_add=True)  # auto_now_add=True: khi tạo một Course, tự động nó sẽ lấy thời điểm hiện tại lúc thêm vô để gán vô biến này mà không cần bạn quan tâm, tự động django sẽ lo
+    updated_date = models.DateTimeField(auto_now=True)  # auto_now=True: nếu mỗi lần có cập nhật liên quan đến Course thì biến này sẽ được cập nhật lại, lấy thời điểm hiện tại mà mình cập nhật
+    active = models.BooleanField(default=True)  # nếu khóa học đó còn thì True, không còn thì False, giống như lms của trường, các HK đã học xong ko phải đã xóa hẳn đi mà nó vẫn được lưu trữ, khi xóa thì chỉ cần tắt (False) đi, để dữ liệu quá khứ và tương lai của mình vẫn còn ổn
+
+
+class Course(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'category')   #Tên (subject) của khóa học (Course) trong cùng một danh mục (Category) không được trùng nhau
+        ordering = ["-id"]
+        #sắp xếp tăng dần theo ID, nếu sắp xếp giảm thì thay "id" bằng "-id"
+        #có thể sắp xếp theo nhiều thuộc tính, ví dụ sắp xếp theo chủ đề và ngày tạo: ordering = ["subject", "created_date"]
+        #ordering này có thể ghi đè lại trong quá trình làm
+
     description = models.TextField(null=True, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)  #auto_now_add=True: khi tạo một Course, tự động nó sẽ lấy thời điểm hiện tại lúc thêm vô để gán vô biến này mà không cần bạn quan tâm, tự động django sẽ lo
-    updated_date = models.DateTimeField(auto_now=True)  #auto_now=True: nếu mỗi lần có cập nhật liên quan đến Course thì biến này sẽ được cập nhật lại, lấy thời điểm hiện tại mà mình cập nhật
-    active = models.BooleanField(default=True)  #nếu khóa học đó còn thì True, không còn thì False, giống như lms của trường, các HK đã học xong ko phải đã xóa hẳn đi mà nó vẫn được lưu trữ, khi xóa thì chỉ cần tắt (False) đi, để dữ liệu quá khứ và tương lai của mình vẫn còn ổn
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    #on_delete=models.CASCADE(): khi xóa Category, toàn bộ Course của Category sẽ xóa theo (mối quan hệ Composition
-    #on_delete=models.SET_NULL(): khi xóa Category, thì trường category này sẽ lấy giá trị null, do đó ta cần để null=True
+    #on_delete=models.CASCADE: khi xóa Category, toàn bộ Course của Category sẽ xóa theo (mối quan hệ Composition
+    #on_delete=models.SET_NULL: khi xóa Category, thì trường category này sẽ lấy giá trị null, do đó ta cần để null=True
+
+
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'course') #Trong cùng một khóa học (Course) không được trùng tên (subject) bài học (Lesson)
+
+    content = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    #on_delete=models.SET_DEFAULT: Khi Course của Lesson bị xóa đi, các bạn muốn cho Lesson này thuộc vào cái Course mặc định
+    #on_delete=models.PROTECT: Cấm --> Khi những Course có những Lesson thì không được xóa những Course đó
